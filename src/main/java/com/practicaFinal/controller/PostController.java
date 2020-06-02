cc799bf58240adb577a843e4327ff6863c49a9ba
yvonnemon <yvosobrero2@hotmail.com>
2020-06-03 00:18:26 +0200

    insert a pincho desde el cliente, pasando parametros con json

--- src/main/java/com/practicaFinal/controller/PostController.java
+++ src/main/java/com/practicaFinal/controller/PostController.java
@@ -1,6 +1,9 @@
 package com.practicaFinal.controller;
 
 
+import com.fasterxml.jackson.databind.node.TextNode;
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
 import com.practicaFinal.manager.PostManager;
 import com.practicaFinal.manager.UserManager;
 import com.practicaFinal.model.Post;
@@ -8,6 +11,8 @@ import com.practicaFinal.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
 
@@ -15,6 +20,9 @@ import java.util.List;
 public class PostController {
     @Autowired
     private PostManager postManager;
+
+    @Autowired
+    private Gson gson;
 
     @Autowired
     private UserManager userManager;
@@ -32,18 +40,29 @@ public class PostController {
     }
 
     @PostMapping("/insert")
-    public String insertPost(){
+    public String insertPost(@RequestBody String body) throws ParseException {
+        JsonObject jsonObject = gson.fromJson(body, JsonObject.class);
+        System.out.println(jsonObject);
+       // String x = jsonObject.get("holi").toString();
+       // System.out.println(x+'8');
+
         Post post = new Post();
-        post.setContent("hola");
-        post.setTranslatedContent("holiwiws");
+        post.setContent(jsonObject.get("content").toString().replaceAll("\"",""));
+        post.setTranslatedContent(jsonObject.get("translatedContent").toString().replaceAll("\"",""));
 
-        post.setLanguage("norcoreano");
-        post.setOriginalLanguage("surcoreano");
+        post.setLanguage(jsonObject.get("language").toString().replaceAll("\"",""));
+        post.setOriginalLanguage(jsonObject.get("originalLanguage").toString().replaceAll("\"",""));
 
-        post.setTitle("titulinsito");
-        post.setTranslatedTitle("titulinsote");
+        post.setTitle(jsonObject.get("title").toString().replaceAll("\"",""));
+        post.setTranslatedTitle(jsonObject.get("translatedTitle").toString().replaceAll("\"",""));
 
-        post.setDatePost(new Date());
+        String fechaString = jsonObject.get("date").toString();
+        fechaString = fechaString.replaceAll("\"","");
+        SimpleDateFormat fechita = new SimpleDateFormat("yyyy-MM-dd");
+        Date f = fechita.parse(fechaString);
+        System.out.println(f);
+
+        post.setDatePost(f);
         post.setUser(userManager.findById(1));
 
         postManager.addOrUpdatePost(post);
