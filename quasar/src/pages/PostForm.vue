6cf2e7b0e2a34089cfa33a5d668ec423be8aded0
yvonnemon <yvosobrero2@hotmail.com>
2020-06-09 18:42:41 +0200

    post con axios y traductor que traduce, todo un exito

--- quasar/src/pages/PostForm.vue
+++ quasar/src/pages/PostForm.vue
@@ -3,16 +3,15 @@
   <q-page class="column">
     <q-input v-model="originalTitle" label="titulo" class="separacion"/>
     <q-input v-model="originalContent" filled type="textarea" label="contenido" class="separacion" />
-    <q-btn color="primary" label="send" @click="send" class="separacion"/>
     
     <label for>idioma original</label>
     <q-select filled v-model="idiomaOriginal" :options="idiomas" option-label="name" option-value="code" class="separacion"/>
-    <label for>idioma a trducir</label>
+    <label for>idioma a traducir</label>
     <q-select filled v-model="idiomaTraducir" :options="idiomas" option-label="name" option-value="code" class="separacion"/>
-        <q-input value="asd" filled type="textarea" label="traducido" />
+    <q-input v-model="translatedContent" filled type="textarea" label="texto traducido" class="separacion" />
 
-    <p>{{idiomaOriginal}} || {{idiomaTraducir}}</p>
-        <q-btn color="primary" label="asdasd" @click="asd" />
+    <q-btn color="primary" label="traducir" @click="traducir" class="separacion"/>
+    <q-btn color="primary" label="send" @click="send" class="separacion"/>
 
   </q-page>
 </template>
@@ -26,11 +25,12 @@ export default {
     return {
       originalTitle: "",
       originalContent: "",
-      originalLanguage: "no",
-      translatedLanguage: "no",
+      originalLanguage: "",
+      translatedLanguage: "",
       translatedTitle: "no",
-      translatedContent: "no",
+      translatedContent: "",
       token: localStorage.getItem("token"),
+
       idiomaTraducir: "",
       idiomaOriginal: "",
       idiomas: []
@@ -45,6 +45,7 @@ export default {
           
       },
     async send() {
+      
       let today = new Date();
       let dd = String(today.getDate()).padStart(2, "0");
       let mm = String(today.getMonth() + 1).padStart(2, "0"); //January is 0!
@@ -53,31 +54,6 @@ export default {
       today = yyyy + "-" + mm + "-" + dd;
 
       console.log(this.token);
-
-      const peticionToken = await fetch("http://localhost:8080/insert", {
-        method: "POST",
-        credentials: "include",
-        headers: new Headers({
-          Authorization: "Bearer " + this.token,
-          "Content-Type": "application/json"
-        }),
-        body: JSON.stringify({
-          title: this.originalTitle,
-          content: this.originalContent,
-          language: this.translatedLanguage,
-          originalLanguage: this.originalLanguage,
-          translatedTitle: this.translatedTitle,
-          translatedContent: this.translatedContent,
-          datePost: today,
-          userId: 1
-        })
-      });
-      let asd = await peticionToken.text();
-      console.log(asd);
-      /* const headers = {
-          'Authorization': "Bearer " + this.token,
-          "Content-Type": "application/json"
-      };
       const data = {
           title: this.originalTitle,
           content: this.originalContent,
@@ -88,28 +64,15 @@ export default {
           datePost: today,
           userId: 1
       };
-      let usuarios = await axios.post("http://localhost:8080/insert", {
-          method: 'POST',
-        credentials: "include",
-        data,
-        headers: headers,
-        /*headers: new Headers({
-          'Authorization': "Bearer " + this.token,
+      let url = 'http://localhost:8080/insert';
+      const axiospost = await axios.post(url,data,{
+        headers: {
+          Authorization: "Bearer " + this.token,
           "Content-Type": "application/json"
-        }),
-        /*params: JSON.stringify({
-          title: this.originalTitle,
-          content: this.originalContent,
-          language: this.translatedLanguage,
-          originalLanguage: this.originalLanguage,
-          translatedTitle: this.translatedTitle,
-          translatedContent: this.translatedContent,
-          datePost: today,
-          userId: 1
-        })
-      });*/
-
-      //console.log(await usuarios.text());
+        }
+      })
+      console.log(axiospost.data);
+      
     },
     async listaridiomas() {
       console.log("traducir");
@@ -132,7 +95,23 @@ export default {
       
     },
     async traducir(){
+        this.originalLanguage = this.idiomaOriginal.name;
+        this.translatedLanguage = this.idiomaTraducir.name;
 
+        let idiomaentrada = this.idiomaOriginal.code;
+        let idiomasalida = this.idiomaTraducir.code;
+        let texto = this.originalContent;
+        let parametros = {
+          'source': idiomaentrada,
+          'target': idiomasalida,
+          'text': texto
+        }
+        const data = {
+          MethodName: 'translate',
+          params: parametros
+        }
+        let traducirPeticion = await axios.post("http://server247.cfgs.esliceu.net/bloggeri18n/blogger.php",data)
+        this.translatedContent = await traducirPeticion.data
     }
   }
 };
